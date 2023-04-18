5cee3cb5e736a7c6419a29be94f90d8918aa87df
sergedoub <5935169+sergedoub@users.noreply.github.com>
2023-04-18 16:56:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def handle_text_message(event):
     logger.info(f'{user_id}: {text}')
 
     try:
-        if text.startswith('/註冊'):
+        if text.startswith('/token'):
             api_key = text[3:].strip()
             model = OpenAIModel(api_key=api_key)
             is_successful, _, _ = model.check_token_valid()
@@ -71,15 +71,15 @@ def handle_text_message(event):
         elif text.startswith('/指令說明'):
             msg = TextSendMessage(text="指令：\n/註冊 + API Token\n👉 API Token 請先到 https://platform.openai.com/ 註冊登入後取得\n\n/系統訊息 + Prompt\n👉 Prompt 可以命令機器人扮演某個角色，例如：請你扮演擅長做總結的人\n\n/清除\n👉 當前每一次都會紀錄最後兩筆歷史紀錄，這個指令能夠清除歷史訊息\n\n/圖像 + Prompt\n👉 會調用 DALL∙E 2 Model，以文字生成圖像\n\n語音輸入\n👉 會調用 Whisper 模型，先將語音轉換成文字，再調用 ChatGPT 以文字回覆\n\n其他文字輸入\n👉 調用 ChatGPT 以文字回覆")
 
-        elif text.startswith('/系統訊息'):
+        elif text.startswith('/role'):
             memory.change_system_message(user_id, text[5:].strip())
-            msg = TextSendMessage(text='輸入成功')
+            msg = TextSendMessage(text='Role Updated')
 
-        elif text.startswith('/清除'):
+        elif text.startswith('/clear'):
             memory.remove(user_id)
-            msg = TextSendMessage(text='歷史訊息清除成功')
+            msg = TextSendMessage(text='History Cleared')
 
-        elif text.startswith('/圖像'):
+        elif text.startswith('/image'):
             prompt = text[3:].strip()
             memory.append(user_id, 'user', prompt)
             is_successful, response, error_message = model_management[user_id].image_generations(prompt)
